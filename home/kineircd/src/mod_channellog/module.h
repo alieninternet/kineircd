0c01c0c681a12970fd2dbf5ed3093e6c4be53365
simonb <>
2003-04-05 14:06:11 +0000

    Fixed up version info - we'll keep the versions standard across all modules and libraries within the package

--- home/kineircd/src/mod_channellog/module.h
+++ home/kineircd/src/mod_channellog/module.h
@@ -1,8 +1,8 @@
 /* $Id$
  * KineIRCd module interface for the channel based logging module
  * 
- * Copyright (c) 2002 Simon Butcher <pickle@alien.net.au>
- * Copyright (c) 2002 KineIRCd Development Team
+ * Copyright (c) 2002,2003 Simon Butcher <pickle@alien.net.au>
+ * Copyright (c) 2002,2003 KineIRCd Development Team
  * (See DEV-TEAM file for details)
  *
  * This file is a part of KineIRCd.
@@ -25,15 +25,19 @@
 #ifndef _SRC_MODULES_MOD_CHANNELLOG_MODULE_H_
 # define _SRC_MODULES_MOD_CHANNELLOG_MODULE_H_ 1
 
+# ifdef HAVE_CONFIG_H
+#  include "autoconf.h"
+# endif
+
 # define MOD_CHANNELLOG_NAME_SHORT		"mod_channellog"
 # define MOD_CHANNELLOG_NAME_LONG		"Channel based logging module"
 
 # define MOD_CHANNELLOG_COPYRIGHT		\
-   "Copyright (c) 2002 KineIRCd Development Team"
+   "Copyright (c) 2002,2003 KineIRCd Development Team"
 
-# define MOD_CHANNELLOG_VERSION_MAJOR		0
-# define MOD_CHANNELLOG_VERSION_MINOR		0
+# define MOD_CHANNELLOG_VERSION_MAJOR		KINEIRCD_VERSION_MAJOR
+# define MOD_CHANNELLOG_VERSION_MINOR		KINEIRCD_VERSION_MINOR
 # define MOD_CHANNELLOG_VERSION_PATCHLEVEL	0
-# define MOD_CHANNELLOG_VERSION_EXTRA		".prealpha"
+# define MOD_CHANNELLOG_VERSION_EXTRA		KINEIRCD_VERSION_EXTRA
 
 #endif // _SRC_MODULES_MOD_CHANNELLOG_MODULE_H_
--- home/kineircd/src/mod_irc2user/module.h
+++ home/kineircd/src/mod_irc2user/module.h
@@ -1,8 +1,8 @@
 /* $Id$
  * KineIRCd module interface for the IRC-2 user protocol module
  * 
- * Copyright (c) 2001,2002 Simon Butcher <pickle@alien.net.au>
- * Copyright (c) 2001,2002 KineIRCd Development Team
+ * Copyright (c) 2001,2002,2003 Simon Butcher <pickle@alien.net.au>
+ * Copyright (c) 2001,2002,2003 KineIRCd Development Team
  * (See DEV-TEAM file for details)
  *
  * This file is a part of KineIRCd.
@@ -25,15 +25,19 @@
 #ifndef _SRC_MODULES_MOD_IRC2USER_MODULE_H_
 # define _SRC_MODULES_MOD_IRC2USER_MODULE_H_ 1
 
+# ifdef HAVE_CONFIG_H
+#  include "autoconf.h"
+# endif
+
 # define MOD_IRC2USER_NAME_SHORT		"mod_irc2user"
 # define MOD_IRC2USER_NAME_LONG			"IRC-2 User Protocol Module"
 
 # define MOD_IRC2USER_COPYRIGHT		\
-   "Copyright (c) 2001,2002 KineIRCd Development Team"
+   "Copyright (c) 2001,2002,2003 KineIRCd Development Team"
 
-# define MOD_IRC2USER_VERSION_MAJOR		0
-# define MOD_IRC2USER_VERSION_MINOR		0
+# define MOD_IRC2USER_VERSION_MAJOR		KINEIRCD_VERSION_MAJOR
+# define MOD_IRC2USER_VERSION_MINOR		KINEIRCD_VERSION_MINOR
 # define MOD_IRC2USER_VERSION_PATCHLEVEL	0
-# define MOD_IRC2USER_VERSION_EXTRA		".prealpha"
+# define MOD_IRC2USER_VERSION_EXTRA		KINEIRCD_VERSION_EXTRA
 
 #endif // _SRC_MODULES_MOD_IRC2USER_MODULE_H_
